c91511ee58ebee4e88a496e83b7c2dd5c0699456
paeore <43215871+paeore@users.noreply.github.com>
2020-02-09 20:19:37 +0000

    Update mjpg.sh

--- mjpg.sh
+++ mjpg.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
-# create def for dir
+# create def for mjpg-streamer-experimental directory
 matchdir="/path/to/mjpg-streamer/mjpg-streamer-experimental"
+
 # move to mjpegstreamer directory
 cd $matchdir
 comand=sudo ./mjpg_streamer -i "./input_uvc.so" -o "./output_http.so"&
